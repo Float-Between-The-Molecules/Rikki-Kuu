a090e08d461f852721208c65b15f69286d2cf2f0
The Anger <anger@floatbetween.space>
2024-12-04 18:12:34 +1100

    update GlobalUsing.cs

--- RikkiKuuGame/GlobalUsing.cs
+++ RikkiKuuGame/GlobalUsing.cs
@@ -4,28 +4,18 @@ Project-wide global using directives.
 please put things here that are needed in the general case only, one-off things should go in the source file that requires it.
 
 as a rule of thumb, godot types are at high risk of overheads due to cross-scripting translations and gdscript's VM.
-it is highly preferable to use C# built-in collection types, threading and so on
+it is highly preferable to use C# built-in collection types, threading and so on.
+in tight bottlenecks it may even end up faster to have local inline implementations.
 */
 
 
 // godot engine
 global using Godot;
 
-// standard system namespaces etc
+// standard system namespaces
 global using System;
 global using System.Runtime.InteropServices;
 global using System.Runtime.CompilerServices;
-
-/*
-frequently used compilation attributes
-
-[MethodImpl(MethodImplOptions.AggressiveInlining)]
-this is roughly the same usage / intention as `inline` keyword in C++
-
-*/
-
-// resolve ambiguity with System.Environment, which is less frequently intended
-global using Environment = Godot.Environment;
 
 // debug
 global using System.Diagnostics;
@@ -35,13 +25,20 @@ global using System.Diagnostics.CodeAnalysis;
 global using System.Threading;
 global using System.Threading.Tasks;
 
-// resolve ambiguity with Godot.Thread, which is seldom intended
-global using Thread = System.Threading.Thread;
-
 // generic programming & collections
 global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
 global using System.Linq;
 global using System.Linq.Expressions;
+
+
+//////// Aliases & Ambiguity Resolution ////////
+
+// resolve ambiguity with System.Environment, which is less frequently intended
+global using Environment = Godot.Environment;
+
+// resolve ambiguity with Godot.Thread, which is seldom intended
+global using Thread = System.Threading.Thread;
 
 // godot generic types shorthand, in the spirit of Godot.GD namespace
 global using GC = Godot.Collections;
