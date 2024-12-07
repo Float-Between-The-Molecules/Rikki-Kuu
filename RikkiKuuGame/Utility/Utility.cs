9e0230a3feba75133df8affb0ab54475d1d74f37
The Anger <anger@floatbetween.space>
2024-12-07 13:41:06 +1100

    cleanup static constructor

--- RikkiKuuGame/Utility/Utility.cs
+++ RikkiKuuGame/Utility/Utility.cs
@@ -11,8 +11,6 @@ public static partial class Utility
 	// static initializer runs at first usage of this class, which should be some bootstrapper in principle
 	static Utility ()
 	{
-		// GD.Print( "RikkiKuu.Utility Static Initializer" );
-		
 		// best place to put a randomize call
 		GD.Randomize();
 		
