c5f89e83d89db7cfa4d155c5ed87e54898b76884
The Anger <anger@floatbetween.space>
2024-12-25 08:54:30 +1100

    refactor godot environment variables

--- RikkiKuuGame/Framework/Framework.cs
+++ RikkiKuuGame/Framework/Framework.cs
@@ -11,24 +11,13 @@ using static Utility;
 public static partial class Framework
 {
 	
-	// project name as reported by godot, not the most reliable property to lean on
-	public static readonly string GodotProjectName;
-	
-	
-	// user directory name as reported by godot, not the most reliable property to lean on
-	public static readonly string GodotProjectDirectory;
-	
-	
 	// is this a case of "mod loads game"?
 	public static readonly bool IsModProjectEnvironment;
 	
 	
-	// is this being loaded with the godot editor?
-	public static readonly bool IsGodotEditorEnvironment;
-	
-	
 	// is the user path that of the game or somewhere else?
-	public static readonly bool IsUserPathWrong;
+	// public static readonly bool IsUserPathWrong;
+	public static readonly bool NonStandardProjectPath;
 	
 	
 	#pragma warning disable CA2255
@@ -36,18 +25,17 @@ public static partial class Framework
 	internal static void _StaticInitializerHook ()
 	{
 		// godot runtime takes issue with module initializers; this is purely for the benefit of the editor
-		if (!IsGodotEditorEnvironment) return;
-		
-		// editor environment will have problems if it tries to look for content in another pack or classes in another dll
-		GD.PushWarning( "Godot Editor Detected - Avoid instantiating non-local content" );
+		if (!GodotEditorFound) return;
 		
 		// _it knows_
 		if (IsModProjectEnvironment) {
-			GD.PushWarning( "Rikki Kuu Modding Detected - Good Fun, Have Luck" );
+			
+			// editor environment will have problems if it tries to look for content in other packs or dll's
+			GD.PushWarning( "Rikki Kuu Modding Detected - Good Fun, Have Luck... Avoid instantiating non-local content" );
 		}
 		
 		// a wrong user dir means it won't access the same settings as the game
-		if (IsUserPathWrong) {
+		if (NonStandardProjectPath) {
 			GD.PushWarning( "Project custom user dir is not set up for live game settings to be available" );
 		}
 	}
@@ -57,14 +45,13 @@ public static partial class Framework
 	// effectively runs just before any symbol in this module is used, including, strangely, the module initializer
 	static Framework ()
 	{
-		// collect data from environment
-		GodotProjectName = ProjectSettings.GetSetting(@"application/config/name").AsString();
-		GodotProjectDirectory = ProjectSettings.GetSetting(@"application/config/custom_user_dir_name").AsString();
-		IsGodotEditorEnvironment = Engine.IsEditorHint() || OS.HasFeature("editor");
 		
-		// extrapolate other properties; not terribly reliable but it's good enough unless you're determined to have a bad time
-		IsUserPathWrong = GodotProjectDirectory != "RikkiKuuGame";
+		// not terribly reliable but it's good enough unless you're determined to have a bad time
 		IsModProjectEnvironment = GodotProjectName != "Rikki Kuu Game";
+		
+		// for modding purposes, it's best to assume the same conditions as the actual game
+		NonStandardProjectPath = GodotProjectDirectory != "RikkiKuuGame";
+		
 	}
 	
 }
--- RikkiKuuGame/Utility/Utility.cs
+++ RikkiKuuGame/Utility/Utility.cs
@@ -8,6 +8,18 @@ namespace RikkiKuu;
 public static partial class Utility
 {
 	
+	// project name as reported by godot
+	public static readonly string GodotProjectName;
+	
+	
+	// user directory name as reported by godot
+	public static readonly string GodotProjectDirectory;
+	
+	
+	// was the godot editor detected
+	public static readonly bool GodotEditorFound;
+	
+	
 	// static initializer runs at first usage of this class, which should be some bootstrapper in principle
 	static Utility ()
 	{
@@ -17,6 +29,11 @@ public static partial class Utility
 		// locate the scene tree at this time
 		Scene = FindSceneTree();
 		Assert( Scene is not null );
+		
+		// collect data about godot environment
+		GodotProjectName = ProjectSettings.GetSetting(@"application/config/name").AsString();
+		GodotProjectDirectory = ProjectSettings.GetSetting(@"application/config/custom_user_dir_name").AsString();
+		GodotEditorFound = Engine.IsEditorHint() || OS.HasFeature("editor");
 	}
 	
 }
